09a7fc8633224b2ac4902240643358e69cc3898e
nandabritto <nanda.britto@gmail.com>
2022-01-04 19:13:13 +0000

    Add blocks on base template, create home app, urls and view

--- store/settings.py
+++ store/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 
     # other apps
+    'home',
 
 ]
 
@@ -54,7 +55,10 @@ ROOT_URLCONF = 'store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
