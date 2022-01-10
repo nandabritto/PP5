aada0c5957bbdba2fb7d749f9ac9f8770b53a904
nandabritto <nanda.britto@gmail.com>
2022-01-10 19:48:51 +0000

    Change view  to redirect to produc detail view

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
-    path("product_details", views.product_detail, name="products")
+    path("product_details/<int:pk>", views.product_detail, name="product_details")
    
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from .forms import UserForm
 
@@ -7,7 +7,9 @@ def products(request):
 
     return render(request, 'products/products.html', {'boxes':boxes})
 
-def product_detail(request):
-    context ={}
+def product_detail(request, pk):
+    context = {
+
+    }
     context['form']= UserForm()
     return render(request, 'products/product_detail.html', context)
