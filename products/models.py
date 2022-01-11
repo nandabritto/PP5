31dd1307dd8d7ae1126af6ade021a974fa286b7f
nandabritto <nanda.britto@gmail.com>
2022-01-11 20:20:07 +0000

    Change Product models

--- products/models.py
+++ products/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 
 class Product(models.Model):
-    boxname = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    product_name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    category = models.ForeignKey(
+        Box, on_delete=models.CASCADE)
     description = models.TextField()
     image = models.ImageField(null=True)
-    product_option1 = models.ImageField(null=True)
-    product_option2 = models.ImageField(null=True)
+
+    def __str__(self):
+        """ Return product name string """
+        return str(self.product_name).lower()
+
+
 
