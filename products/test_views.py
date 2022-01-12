6138dddd0e65205290909814b42fc767ba502ffa
nandabritto <nanda.britto@gmail.com>
2022-01-12 23:10:03 +0000

    Create product views unittests

--- products/test_views.py
+++ products/test_views.py
@@ -9,17 +9,17 @@ class TestViews(TestCase):
         """
         This method runs before the execution of each test case.
         """
-        Box.objects.create(box_name='BoxTest')
+        self.boxtest = Box.objects.create(box_name='BoxTest', category='test', box_price='49.99', box_description='Test' , box_image='noimage.jpg')
                 
         
     def test_boxes_view(self):
         """ Test redirect if user is invalid is correct """
-        boxes = Box.objects.all()
         response = self.client.get(reverse('boxes'))
         self.assertEqual(response.status_code, 200)
 
 
     def test_product_detail_view(self):
-        # response = self.client.get(reverse('product_detail', kwargs={
-        #     'pk': 1}))
-        # self.assertEqual(response.status_code, 200)
+        url = reverse('product_details', kwargs={
+            'pk': self.boxtest.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
