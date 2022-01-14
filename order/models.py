8e934e93876e78d17bbb3672a65e41e11e521027
nandabritto <nanda.britto@gmail.com>
2022-01-14 20:47:19 +0000

    Add shipping class on order app

--- order/models.py
+++ order/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from products.models import Box
 
 
 class Order(models.Model):
@@ -11,4 +12,24 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
-    
+
+
+class OrderBox(models.Model):
+    box = models.ForeignKey(Box, on_delete=models.SET_NULL, blank=True, null=True)
+    order_box = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address1 = models.CharField(max_length=100, null=True)
+    addres2 = models.CharField(max_length=100, null=True)
+    county = models.CharField(max_length=20, null=True)
+    country = models.CharField(max_length=30, null=True)
+    eircode = models.CharField(max_length=7, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+            return str(self.address1)
