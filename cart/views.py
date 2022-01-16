8db9aa847273e8aae2dfadc992a583b9635e383e
nandabritto <nanda.britto@gmail.com>
2022-01-16 18:37:43 +0000

    Create cart badge on cart page

--- cart/views.py
+++ cart/views.py
@@ -15,12 +15,15 @@ def cart(request):
         order, created = Order.objects.get_or_create(
             customer=customer, complete=False)
         items = order.orderbox_set.all()
+        cartItems = order.get_cart_items
+
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
 
-    context = {'items': items, 'order': order,}
-
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
+ 
     return render(request, 'cart/cart.html', context)
 
 def updateCart(request):
