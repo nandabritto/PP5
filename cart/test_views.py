601487ed191e80754c437be8586e4a779e4a98a7
nandabritto <nanda.britto@gmail.com>
2022-01-17 19:15:18 +0000

    Add unittests for cart function on cart app

--- cart/test_views.py
+++ cart/test_views.py
@@ -1,7 +1,9 @@
 """ System Module """
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.contrib import auth
+from products.models import Box
 import logging
 
 
@@ -9,7 +11,7 @@ class SetupViewTestCase(TestCase):
     """
     Test cart views
     """
-    def setup(self):
+    def setUp(self):
         self.username = 'joe'
         self.password = '12345'
         user = User.objects.create_user(
@@ -17,12 +19,32 @@ class SetupViewTestCase(TestCase):
             email='joe@doe.com',
             password=self.password)
         self.client.login(username='joe', password='12345')
-        logging.debug(self.username)
+        # logging.debug(self.username)
+        self.box = Box.objects.create(
+            box_name = 'Test Box',
+            box_price = '10.99',
+            category = 'Test Category',
+            box_description = 'test Description',
+            box_image = 'testimage.jpg')
+             
     
-
-
 class TestCart(SetupViewTestCase):
     def test_if_user_is_autheticated(self):
+        self.client.login(username='joe', password='12345')
         user = auth.get_user(self.client)
-        self.assertIn('joe', self.client.session)
+        # logging.debug(user)
         
+    def test_cart_view_url_if_authenticated(self):
+        """
+        Test response on cart view if user is authenticated
+        """
+        response = self.client.get(reverse('cart'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_cart_view_url_if_not_authenticated(self):
+        """
+        Test response on cart view if user is not authenticated
+        """
+        self.client.logout()
+        response = self.client.get(reverse('cart'))
+        self.assertEqual(response.status_code, 200)
