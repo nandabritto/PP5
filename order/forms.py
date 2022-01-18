5801943306c9639c0d565b5dcd3b02057eb3b362
nandabritto <nanda.britto@gmail.com>
2022-01-18 20:35:44 +0000

    Create checkout views and html

--- order/forms.py
+++ order/forms.py
@@ -1,14 +1,23 @@
+""" System Module """
 from django import forms
 from django_countries.fields import CountryField
 
-class CheckoutForm(forms.Form):
-    address1 = forms.CharField()
-    address2 = forms.CharField(required=False)
-    county = forms.CharField()
-    country = CountryField(blank_label='(select country)')
-    eircode = forms.CharField()
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect())
- 
 
+class CheckoutForm(forms.Form):
+    """
+    Create a shipping address form
+    """
+    address1 = forms.CharField(
+        label="Address 1",
+        widget=forms.TextInput(attrs={'placeholder': 'Apartment 1'      
+    }))
+    address2 = forms.CharField(
+        label="Address 2",
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Dublin'}))
+    county = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Dublin'}))
+    country = CountryField(blank_label='(select country)').formfield()
+    eircode = forms.CharField()
+    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
+    save_info = forms.BooleanField(widget=forms.CheckboxInput(),required=False)
--- order/views.py
+++ order/views.py
@@ -1,5 +1,5 @@
 """ System Module """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import CheckoutForm
 from .models import Order
@@ -22,28 +22,30 @@ class CheckoutView(View):
 
     def post(self, *args, **kwargs):
         form = CheckoutForm(self.request.POST or None)
+        print(self.request.POST)
         if form.is_valid():
+            print(form.cleaned_data)
             print('The form is valid')
             return redirect('checkout')
 
 
 
-# def checkout(request):
-#     """
-#     A view to return checkout page
-#     """
-    # if request.user.is_authenticated:
-    #     customer = request.user
-    #     order, created = Order.objects.get_or_create(
-    #         customer=customer, complete=False)
-    #     items = order.orderbox_set.all()
-    #     cartItems = order.get_cart_items
+    # def checkout(request):
+    #     """
+    #     A view to return checkout page
+    #     """
+    #     if request.user.is_authenticated:
+    #         customer = request.user
+    #         order, created = Order.objects.get_or_create(
+    #             customer=customer, complete=False)
+    #         items = order.orderbox_set.all()
+    #         cartItems = order.get_cart_items
 
-    # else:
-    #     items = []
-    #     order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': True}
-    #     cartItems = order['get_cart_items']
+    #     else:
+    #         items = []
+    #         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': True}
+    #         cartItems = order['get_cart_items']
 
-    # context = {'items': items, 'order': order, 'cartItems': cartItems}
+    #     context = {'items': items, 'order': order, 'cartItems': cartItems}
 
-    # return render(request, 'order/checkout.html')
+    #     return render(request, 'order/checkout.html')
