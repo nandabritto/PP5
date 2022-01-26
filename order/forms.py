d317d1e6e58949051875be7e3498937abbc60688
nandabritto <nanda.britto@gmail.com>
2022-01-26 22:12:36 +0000

    Add js to hide shipping and billing address

--- order/forms.py
+++ order/forms.py
@@ -17,10 +17,13 @@ class CheckoutForm(forms.Form):
         required=False,
         widget=forms.TextInput(attrs={'placeholder': 'Dublin'}))
     shipping_county = forms.CharField(
+        label="County",
         required=False,
         widget=forms.TextInput(attrs={'placeholder': 'Dublin'}))
     shipping_country = CountryField(blank_label='(select country)').formfield(required=False)
-    shipping_eircode = forms.CharField(required=False,)
+    shipping_eircode = forms.CharField(
+        label="EirCode",
+        required=False)
 
     billing_address1 = forms.CharField(
         label="Address 1",
@@ -40,11 +43,20 @@ class CheckoutForm(forms.Form):
 
 
     same_billing_address = forms.BooleanField(
+        label="Billing address is the same as my shipping address",
         widget=forms.CheckboxInput(), required=False)
 
-    set_default_shipping = forms.BooleanField(required=False)
-    use_default_shipping = forms.BooleanField(required=False)
+    set_default_shipping = forms.BooleanField(
+        label="Save default shipping address",
+        required=False)
+    use_default_shipping = forms.BooleanField(
+        label="Use default shipping address",
+        required=False)
 
     
-    set_default_billing = forms.BooleanField(required=False)
-    use_default_billing = forms.BooleanField(required=False)
+    set_default_billing = forms.BooleanField(
+        label="Set default billing address",
+        required=False)
+    use_default_billing = forms.BooleanField(
+        label="Use default billing address",
+        required=False)
