036b91878c58eeffe1f1fcf45111dd00baa70236
nandabritto <nanda.britto@gmail.com>
2022-01-31 20:09:27 +0000

    Create profile urls and views

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,11 +1,10 @@
-# from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
-# @login_required
-# def profile(request):
-#     return render(request, 'user_profile/profile.html')
-#     context = {}
+def profile(request):
+    template = 'user_profile/profiles.html'
+    context = {}
 
-#     return render request, template, context)
+    return render(request, template, context)
 
