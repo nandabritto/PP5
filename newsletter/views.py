1499bb9af89d3850a211de30ea7e9c26f2eb8a7c
nandabritto <nanda.britto@gmail.com>
2022-02-03 22:50:32 +0000

    Create newsletter unsubscribe  view

--- newsletter/views.py
+++ newsletter/views.py
@@ -20,5 +20,26 @@ def newsletter_signup(request):
     context = {
         'form: form',
     }
-    template = 'newsletters/signuo.html'
+    template = 'newsletters/signup.html'
     return render(request, template, context)
+
+
+def newsletter_unsubscribe(request):
+    """
+    Creates newsletter unsubscribe view and get the form
+    """
+    form = NewsLetterUserSignUpForm(request.POST or None):
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if NewsletterUser.objects.filter(email=instance.email).exist():
+            NewsletterUser.objects.filter(email=instance.email).delete()
+        else:
+            messages.warning(request, 'Sorry, We did not find your email.')
+
+    context = {
+        'form: form',
+    }
+    template = 'newsletters/unsubscribe.html'
+    return render(request, template, context)
+
