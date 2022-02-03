645d5c9b72a7ed42d8d65093bca93efc8f7f0e53
nandabritto <nanda.britto@gmail.com>
2022-02-03 22:33:26 +0000

    Add newsletter model and admin

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class NewsletterUser(models.Model):
+    """
+    Create newsletter model with email and date
+    """
+    email = models.EmailField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
