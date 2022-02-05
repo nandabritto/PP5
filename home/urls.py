a68b876230e2aa486fccef0c416c73294c3216c3
nandabritto <nanda.britto@gmail.com>
2022-02-05 20:15:44 +0000

    Add management page with links to admin

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from .views import Management
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('management/', Management.as_view(), name='management'),
 ]
