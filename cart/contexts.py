bc18fc7310d6b2b4f7cd811314bfdfa9810d784a
nandabritto <nanda.britto@gmail.com>
2022-02-15 17:20:05 +0000

    Add color to alert in payment page

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,30 +1,20 @@
+""" System Module """
 from order.models import Order
-# def cart_contents(request):
-#     """
-#     Creates cart items context
-#     """
-#     cart_items = request.session.get('cart_items', {})
-#     context = {
-#                 'cart_items': cart_items
-#             }
-#     return context
 
 
 def cart_contents(request):
     """
     Creates order contexts to be callable in profile pages
     """
-    cartItemsCount = 0
+    cart_items_count = 0
     if request.user.is_authenticated:
         order = Order.objects.filter(
             customer=request.user, ordered=False)
         if order.exists():
             open_order = order[0]
-            cartItemsCount = open_order.get_cart_items
+            cart_items_count = open_order.get_cart_items
 
     context = {
-                'cartItemsCount': cartItemsCount
+                'cart_items_count': cart_items_count
             }
-       
     return context
-    
