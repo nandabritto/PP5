66218db98771c21bbb2de96b782c7a2d819506c1
nandabritto <nanda.britto@gmail.com>
2022-02-15 22:49:52 +0000

    Fix bug on checkout js file

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -6,21 +6,23 @@ var use_default_shipping = document.querySelector("input[name=use_default_shippi
 var use_default_billing = document.querySelector("input[name=use_default_billing]");
 var same_billing_address = document.querySelector("input[name=same_billing_address]");
 
+if(use_default_shipping){
 use_default_shipping.addEventListener('change', function() {
   if (this.checked) {
     hideable_shipping_form.hide();
   } else {
     hideable_shipping_form.show();
   }
-})
+})}
 
+if(use_default_billing){
 use_default_billing.addEventListener('change', function() {
   if (this.checked) {
     hideable_billing_form.hide();
   } else {
     hideable_billing_form.show();
   }
-})
+})}
 
 same_billing_address.addEventListener('change', function() {
     if (this.checked) {
